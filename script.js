e5b0f018acb0c9085b53c888488bc6f41efeb752
Radmir <radmir_em@bigmir.net>
2019-07-03 13:30:41 +0300

    All tasks are completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-let money = prompt("Ваш бюджет намесяц?", "");
+let money = prompt("Ваш бюджет намесяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let q1 = prompt("Введите обязательную статью расходов в этом месяце",''),
+q2 = prompt("Во сколько обойдется?", ''),
+q3 = prompt("Введите обязательную статью расходов в этом месяце",''),
+q4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.q1 = q2;
+appData.expenses.q3 = q4;
+
+alert(appData.budget/30);
+
+
